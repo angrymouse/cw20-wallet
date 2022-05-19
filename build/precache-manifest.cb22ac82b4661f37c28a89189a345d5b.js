69d61ad8cb850d5345a03a1abaf0735b9eae6357
Nik Rykov <rykov.nikas@gmail.com>
2022-05-18 19:03:34 -0700

    build dif from gitignore

---
+++ build/precache-manifest.cb22ac82b4661f37c28a89189a345d5b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ae4ce6929a2e0c3bdcd132b6fa9982b",
+    "url": "/cw20-wallet/index.html"
+  },
+  {
+    "revision": "efecaac516ff35441c86",
+    "url": "/cw20-wallet/static/css/2.6a4a3e77.chunk.css"
+  },
+  {
+    "revision": "e59da1ba278b42e90796",
+    "url": "/cw20-wallet/static/css/main.6380e5c8.chunk.css"
+  },
+  {
+    "revision": "efecaac516ff35441c86",
+    "url": "/cw20-wallet/static/js/2.cf09865d.chunk.js"
+  },
+  {
+    "revision": "b1f248559ba67264dcc58e82c33d7fd4",
+    "url": "/cw20-wallet/static/js/2.cf09865d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e59da1ba278b42e90796",
+    "url": "/cw20-wallet/static/js/main.cf664f5b.chunk.js"
+  },
+  {
+    "revision": "827c99e6165bbcdd01ff",
+    "url": "/cw20-wallet/static/js/runtime-main.0f114c7a.js"
+  },
+  {
+    "revision": "df8732334b3659e59d4d65abbba33f72",
+    "url": "/cw20-wallet/static/media/backArrow.df873233.svg"
+  },
+  {
+    "revision": "d3695d54ce885fef2a9368b67cad5d5a",
+    "url": "/cw20-wallet/static/media/cosmWasmLogo.d3695d54.svg"
+  },
+  {
+    "revision": "fdc2900047379c6047b841b34de0a8e3",
+    "url": "/cw20-wallet/static/media/edit.fdc29000.svg"
+  },
+  {
+    "revision": "71513020fdd4db4321b81c43daf03518",
+    "url": "/cw20-wallet/static/media/failIcon.71513020.svg"
+  },
+  {
+    "revision": "2e305b6693ad95cd56039373d2051bc2",
+    "url": "/cw20-wallet/static/media/successIcon.2e305b66.svg"
+  }
+]);
